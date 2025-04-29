4df0ea58dd90a2397fcf488241ad401e0200fd6b
Tubagus Aulia <tebe.aulia@gmail.com>
2025-04-29 15:08:37 +0700

    penambahan model

--- TubesKPL/obj/Debug/net8.0/TubesKPL.AssemblyInfo.cs
+++ TubesKPL/obj/Debug/net8.0/TubesKPL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TubesKPL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c44dbac072088c8d423d0a0f49fff77e814320b")]
 [assembly: System.Reflection.AssemblyProductAttribute("TubesKPL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TubesKPL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
