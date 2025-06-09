8fcd2fadb4dfaa43f828149da763c4ab12d94293
Irfan Rangga <irfanrangga04@gmail.com>
2025-06-09 13:48:09 +0700

    menambahkan GUI Homepage

--- ManajemenPerpus.GUI/MenuUtama.cs
+++ ManajemenPerpus.GUI/MenuUtama.cs
@@ -16,5 +16,35 @@ namespace ManajemenPerpus.GUI
         {
 
         }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void customButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LogoutButton_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
